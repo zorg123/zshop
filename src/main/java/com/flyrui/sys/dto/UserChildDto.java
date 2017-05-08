19e0edb728ede60af3e2e180a647aa34c717ecc7
lixikun <lixikun@hotmail.com>
2017-05-08 17:53:10 +0800

    a

---
+++ src/main/java/com/flyrui/sys/dto/UserChildDto.java
@@ -0,0 +1,28 @@
+package com.flyrui.sys.dto;
+
+public class UserChildDto {
+	
+	private Integer cid;
+	private Integer id;
+	private Integer nlevel;
+	
+	public Integer getCid() {
+		return cid;
+	}
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getNlevel() {
+		return nlevel;
+	}
+	public void setNlevel(Integer nlevel) {
+		this.nlevel = nlevel;
+	}
+	
+}
